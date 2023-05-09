2338d6ab286844375944630ac146465f71334774
Luciano Martins <luciano.martinsjr@hotmail.com>
2023-05-09 19:54:41 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteComponent } from './componente/componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteComponent
   ],
   imports: [
     BrowserModule,
